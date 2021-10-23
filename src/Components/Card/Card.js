d2e0358343fa64428306872a45ebfe4d1f7b80db
BibhushanKarki <bibhushan.karki11@gmail.com>
2021-10-23 15:39:51 +0545

    board,card,dropdown and editable component functional

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { MoreHorizontal } from "react-feather";
+
+import Dropdown from "../Dropdown/Dropdown";
+
+import "./Card.css";
+
+function Card(props) {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const { id, title, tasks, labels } = props.card;
+
+
+  return (
+    <>
+      <div
+        className="card"
+        draggable
+        onDragEnd={() => props.dragEnded(props.boardId, id)}
+        onDragEnter={() => props.dragEntered(props.boardId, id)}
+      >
+        <div className="card_top">
+           <div
+            className="card_top_more"
+            onClick={(event) => {
+              event.stopPropagation();
+              setShowDropdown(true);
+            }}
+          >
+            <MoreHorizontal />
+            {showDropdown && (
+              <Dropdown
+                class="board_dropdown"
+                onClose={() => setShowDropdown(false)}
+              >
+                <p onClick={() => props.removeCard(props.boardId, id)}>
+                  Delete Card
+                </p>
+              </Dropdown>
+            )}
+          </div>
+        </div>
+        <div className="card_title">{title}</div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
